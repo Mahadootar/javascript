2131c4abe6dd6caa81c83a52347d21951bc783fd
mahadyussufnur <mahad-nur-yussuf@outlook.de>
2025-06-22 15:32:28 +0200

    I am learning javascript

---
+++ strings/script.js
@@ -0,0 +1,51 @@
+//diffrent ways to create strings
+
+//let age = 20;
+//let firstName = `mahad is ${age}`;
+
+//console.log(firstName);
+
+
+let firstName = "John J"; // using double quotes
+let lastName = 'doe'; // using single quotes
+let age = 30;;
+let isMarried = false;
+let city = new String("New york"); // using new constructor
+let country = `USA`; // using template literals back ticks
+//console.log(firstName, lastName, city.toString(), country);
+
+// how to access strings 
+
+//console.log(firstName.indexOf("J"));
+//console.log(firstName.indexOf("n"));
+//console.log(firstName.lastIndexOf("J"));
+
+
+// string methods 
+let str = "Hi world, how are you? and you";
+//console.log(str.length);
+//console.log(str.toUpperCase());
+//console.log(str.toLowerCase());
+//console.log(str.trim());
+//console.log(str.trimStart());
+//console.log(str.trimEnd());
+//console.log(str.split(","));
+//console.log(str.split(" "));
+//console.log(str.replace("Hello", "Hi"));
+//console.log(str.includes("baby"));
+//console.log(str.startsWith("Hello"));
+//console.log(str.endsWith("you?"));
+//console.log(str.repeat(3));
+//console.log(str.padStart(200, "hello"));
+//console.log(str.charAt(0));
+//console.log(str.charCodeAt(0));
+//console.log(str.slice(0, 5));
+//console.log(str.substring(0, 5));
+//console.log(str.substring(7, 12));
+//console.log(str.split(" ").join(" "));
+
+//if (str.includes("world")) {
+    //console.log("this string has word world");
+//} else {
+  //  console.log("this string does not have word world");
+//}
